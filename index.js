6dc2547f4d1c739d9992f228e1fff325a2cf77ca
praveen96kumar <118809623+praveen96kumar@users.noreply.github.com>
2023-06-27 22:18:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+document.querySelector("button").addEventListener("click", roll);
+
+function roll () {
+var random1 = Math.floor((Math.random()*6)+1);
+var random2 = Math.floor((Math.random()*6)+1);
+
+document.querySelector(".img1").setAttribute("src" ,"images/dice"+random1+".png");
+document.querySelector(".img2").setAttribute("src" ,"images/dice"+random2+".png");
+
+if (random1>random2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins !";
+} else if (random2>random1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins !";
+} else {
+    document.querySelector("h1").innerHTML = "Draw !";
+}
+}
